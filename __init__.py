61e1d8c1af8ce304bddac6fe67512857ecaae409
unclemusclez <unclemusclez@gmail.com>
2025-12-06 16:35:42 -0500

    luma.

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from luma.luma import Luma
 import luma.api_client
-import luma.models.calendar_get as calendar_get
-import luma.models.data_models as data_models
+import luma.models.calendar_get
+import luma.models.data_models
 
 def setup(bot):
     bot.add_cog(Luma(bot))
--- luma.py
+++ luma.py
@@ -1,17 +1,17 @@
 import asyncio
 import logging
 from datetime import datetime
-
+from datetime import timedelta
+from typing import Dict, List
 import discord
 from redbot.core import Config, commands, checks
 from redbot.core.bot import Red
 from redbot.core.utils import menus
-from datetime import timedelta
-from typing import Dict, List
-from calendar_get import Event
 
-from data_models import Subscription, ChannelGroup
-from api_client import (
+from luma.models.calendar_get import Event
+
+from luma.models.data_models import Subscription, ChannelGroup
+from luma.api_client import (
     LumaAPIClient,
     LumaAPIError,
     LumaAPIRateLimitError,
