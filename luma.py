10c8496a49c112d3d839199418c68b6dfe11256b
unclemusclez <unclemusclez@gmail.com>
2025-12-06 16:18:18 -0500

    ?

--- luma.py
+++ luma.py
@@ -8,10 +8,10 @@ from redbot.core.bot import Red
 from redbot.core.utils import menus
 from datetime import timedelta
 from typing import Dict, List
-from luma.models.calendar_get import Event
+from .models.calendar_get import Event
 
-from luma.models.data_models import Subscription, ChannelGroup
-from luma.api_client import (
+from .models.data_models import Subscription, ChannelGroup
+from .api_client import (
     LumaAPIClient,
     LumaAPIError,
     LumaAPIRateLimitError,
